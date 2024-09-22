a814908636e1847c2a426b81189c9c91c8e38b52
Divi987 <divtesting987@gmail.com>
2024-09-22 20:18:36 +0545

    Fix: Add RecoilWrapper in layout file

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import ApplicationButton from "@/components/ApplicationButton";
 import SideBar from "@/components/SideBar";
+import RecoilRootWrapper from "./RecoilRootWrapper";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,12 +17,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <RecoilRootWrapper>
         <Header />
         <ApplicationButton />
         <main className="flex w-full" style={{ height:"600px" }}>
           <SideBar />
           {children}
+          {/* {children} */}
         </main>
+        </RecoilRootWrapper>
       </body>
     </html>
   );
