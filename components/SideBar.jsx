0e0f15a0c96d12183999e48aba902f00b8136a0b
Divi987 <divtesting987@gmail.com>
2024-09-23 06:03:45 +0545

    Update sidebar links

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -4,14 +4,14 @@ import style from './styles.module.css'
 
 export default function SideBar(){
     return(
-        <div className="w-1/5 p-[5px]">
-            <ul style={{ fontSize: "small"}}>
-                <li className={style.NormalRow}> <Link className="TreeLinksText" href="#"> Apply Now </Link></li>
+        <div className="w-2/5 lg:w-1/5 p-[5px]">
+            <ul className="text-[12px] lg:text-small">
+                <li className={`${style.NormalRow}`}> <Link className="TreeLinksText" href="/"> Apply Now </Link></li>
                 <li className={style.AltTableRow}><Link className="TreeLinksText" href="/AVATS/retrieveApplication.aspx"> Retreive Application</Link></li>
-                <li className={style.NormalRow}><Link className="TreeLinksText" href="#">Frequently Asked Questions</Link></li>
-                <li className={style.AltTableRow}><Link className="TreeLinksText" href="#"> Terms & conditions</Link></li>
-                <li className={style.NormalRow}><Link className="TreeLinksText" href="#">Privacy Statement & Cookies</Link></li>
-                <li className={style.AltTableRow}><Link className="TreeLinksText" href="#">Freedom of Information</Link></li>
+                <li className={style.NormalRow}><Link className="TreeLinksText" href="/AVATS/frequently-asked-questions">Frequently Asked Questions</Link></li>
+                <li className={style.AltTableRow}><Link className="TreeLinksText" href="/AVATS/terms-conditions"> Terms & conditions</Link></li>
+                <li className={style.NormalRow}><Link className="TreeLinksText" href="/AVATS/privacy-statement-Cookies">Privacy Statement & Cookies</Link></li>
+                <li className={style.AltTableRow}><Link className="TreeLinksText" href="/AVATS/freedom-of-information">Freedom of Information</Link></li>
             </ul>
         </div>
     )
