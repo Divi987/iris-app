b89b438ac38a447cfd41ce97b1b5c9f31218d037
Divi987 <divtesting987@gmail.com>
2024-09-23 06:01:02 +0545

    Add informative pages

---
+++ app/AVATS/terms-conditions/page.jsx
@@ -0,0 +1,48 @@
+export default function visaStatus() {
+    return(
+        <>
+            <div>
+			<div width="428" height="57" valign="top">
+				<p className="MainHeadersText">Terms and Conditions</p>
+				<p>The Online Visa/Preclearance Application facility is maintained for your personal use and viewing by the Immigration Service Delivery. Access and use by you of this site constitutes acceptance by you of these Terms and Conditions.
+                </p><p>
+                Throughout this document the terms �we�, �us� and �our� refer to the Immigration Service Delivery, and the Department of Justice, Equality and Law Reform. The terms �you� and �your� refer to you the user.
+                </p>
+                <p className="SubHeadersText">
+                Copyright
+                </p><p>
+                Material featured on this site is the copyright of the Department of Justice, Equality and Law Reform unless otherwise indicated. 
+                </p><p>                
+                The Department of Justice, Equality &amp; Law Reform complies with the Re-use of Public Sector Information Regulations 2005 (SI 279 of 2005).
+                </p><p>
+                You may re-use the information on this website free of charge, in any format, provided it is reproduced accurately and not used in a misleading way.
+                </p><p>
+                Re-use includes copying, issuing copies to the public, publishing, broadcasting and translating into other languages. It also covers non-commercial, research and study.
+                </p><p>
+                You must acknowledge the source and our copyright in cases where you supply the information to others.
+                </p><p>
+                You may not use the information for the principal purpose of advertising or promoting a particular product or service, or for illegal, immoral, fraudulent, or dishonest purposes.
+                </p><p>
+                The Department of Justice, Equality and Law Reform is not liable for any loss or liability associated with the re-use of information and does not certify that the information is up to date or error free.
+                </p><p>
+                No user is authorised to have exclusive rights to re-use information contained on this site.
+                </p>
+                <p className="SubHeadersText">
+                Virus Protection
+                </p><p>
+                We make every effort to check and test material at all stages of production. It is always wise for you to run an anti-virus program on all material downloaded from the Internet. We cannot accept any responsibility for any loss, disruption or damage to your data or your computer system which may occur whilst using material derived from this website.
+                </p>
+                <p className="SubHeadersText">
+                Links to Other Websites
+                </p><p>
+                We are not responsible for the content or reliability of any website to which we provide a link. We cannot guarantee these links will work all of the time and we have no control over the availability of linked pages.
+                </p>
+                <p className="SubHeadersText">
+                Disclaimer
+                </p><p>
+                The information contained on these pages is intended as a guide only. While every effort is made to ensure all information is accurate and up to date, no responsibility is accepted by or on behalf of the State for any errors, omissions, or misleading statements on these pages or any site to which these pages connect.
+                </p>			</div>
+		</div>
+        </>
+    )
+}
