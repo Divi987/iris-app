778527e72ce58262ec9fc71d49badbf257b636b4
Divi987 <divtesting987@gmail.com>
2024-09-23 06:03:12 +0545

    Add api request method to onClick

--- components/ApplicationButton.jsx
+++ components/ApplicationButton.jsx
@@ -1,8 +1,13 @@
 'use client'
 import { usePathname } from 'next/navigation';
 import style from './styles.module.css';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { userDobStateSelector, userNationalityStateSelector, userPassportStateSelector, userTranRecordNumberStateSelector } from '@/recoil/selectors/selectors';
+import { fetchItemDetails } from '@/lib/api/fetchReq';
+import { setCookie } from 'cookies-next';
+import { userState } from '@/recoil/atoms/states';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation' 
 
 export default function ApplicationButton(){
     const pathName = usePathname();
@@ -11,16 +16,49 @@ export default function ApplicationButton(){
     const transactionRecoNumberValue = useRecoilValue(userTranRecordNumberStateSelector);
     const dobValues = useRecoilValue(userDobStateSelector);
     const nationalityValue = useRecoilValue(userNationalityStateSelector);
+    const setUsers = useSetRecoilState(userState);
+    const [message, setMessage] = useState("");
+    let router = useRouter();
 
-    const handleSubmit = () => {
+
+    const handleSubmit = async () => {
+
         console.log(transactionRecoNumberValue, passportRecoilValue, dobValues, nationalityValue);
+
+        let fetchData = await fetchItemDetails(passportRecoilValue);
+        console.log(fetchData);
+        // setShouldFetch(true);
+    
+        if (fetchData.user) {
+            const result = fetchData.user;
+            if (
+                result.grantNumber === transactionRecoNumberValue &&
+                result.passportNumber === passportRecoilValue &&
+                result.passportNationality === nationalityValue
+              ) {
+                setUsers(fetchData.user);
+                const cookieJson = fetchData.user;
+                setCookie("cookieKey", JSON.stringify(fetchData.user), {maxAge: 60*10 });
+                setCookie("pKey", JSON.stringify(result.passportNumber), {maxAge: 60*10 });
+                router.push("/AVATS/visa-status.aspx");
+              } else {
+                setMessage("Record not found!");
+              }
+        } else {
+            setMessage("Record Not Found!");
+        }
+    
+
     }
 
     return(
+        <>
+        {message}
         <div className={style.applicationButtonBar}>
             <div className="p-px">
                 <button onClick={handleSubmit} className={`${pathName !== '/AVATS/retrieveApplication.aspx' ? 'hidden' : `inline-block box-border  ${style.ApplicationButtons}`}`} >submit</button>
             </div>
         </div>
+        </>
     )
 }
